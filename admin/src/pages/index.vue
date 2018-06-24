88c9d6bf01f6a86e7802d0c6f6c842b60df190f0
timoxa <timoxa17@gmail.com>
2018-06-24 09:23:15 +0300

    Readme

--- admin/src/pages/index.vue
+++ admin/src/pages/index.vue
@@ -1,8 +1,15 @@
 <template lang="pug">
 	page.page-displays
 		span(slot="title") Administrative interface demo
-		p Demo site for <a href="https://github.com/mrTimofey/vue-admin" target="_blank">vue-admin-front</a> and <a href="https://github.com/mrTimofey/laravel-admin-api">mr-timofey/laravel-admin-api</a>.
-		p Use aside links to learn more.
+		.lead
+			p Demo site for <a href="https://github.com/mrTimofey/vue-admin" target="_blank">vue-admin-front</a> and <a href="https://github.com/mrTimofey/laravel-admin-api" target="_blank">mr-timofey/laravel-admin-api</a>.
+			p Use aside links to learn more.
+			br
+			a.btn.btn-primary(href="https://github.com/mrTimofey/admin-demo" target="_blank") Demo source code
+			!=' '
+			a.btn.btn-primary(href="https://mr-timofey.gitbooks.io/vue-admin/" target="_blank") vue-admin-front docs
+			!=' '
+			a.btn.btn-primary(href="https://github.com/mrTimofey/laravel-admin-api" target="_blank") laravel-admin-api docs
 </template>
 <style lang="stylus">
 	.page-displays
--- readme.md
+++ readme.md
@@ -1,1 +1,7 @@
 # AdminPanel demo page
+
+This is a demo project illustrating features of
+[vue-admin-front](https://github.com/mrTimofey/vue-admin) with Laravel 5 and
+[mr-timofey/laravel-admin-api](https://github.com/mrTimofey/laravel-admin-api).
+
+[Live demo](http://admin.shit-free.space)
